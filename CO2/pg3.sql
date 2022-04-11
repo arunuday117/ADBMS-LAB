882b0f2d2b238ecb9ec2f1a50a1b1d1a5a188867
Arun Uday <72249990+arunuday117@users.noreply.github.com>
2022-04-11 16:02:29 +0530

    Update pg3.sql

--- CO2/pg3.sql
+++ CO2/pg3.sql
@@ -7,9 +7,15 @@ insert into employee values(110,10000);
 select emp_id,incent(emp_id),salary from employee;
 
 //function
-create database emp;
-use emp;
-create table employee(emp_id INT,salary FLOAT);
-insert into employee values(1,20000);
-insert into employee values(110,10000);
-select emp_id,incent(emp_id),salary from employee;
+CREATE DEFINER=`root`@`localhost` FUNCTION `incent`(id INT) RETURNS int(11)
+BEGIN
+DECLARE 
+	incentive FLOAT;
+IF(id = 110)
+THEN
+	set incentive = 1000;
+ELSE 
+	SET incentive =0;
+END IF;
+RETURN incentive;
+END
